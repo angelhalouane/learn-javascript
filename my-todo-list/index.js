bd7bcde47c0d52610d8def63412bc23ee16bf646
Angel Amelia Halouane <angeleads@Angels-MacBook-Pro.local>
2021-09-20 13:46:17 +0200

    modified some implicit names to give them a better understanding of their use

--- my-todo-list/index.js
+++ my-todo-list/index.js
@@ -6,27 +6,31 @@ loopTaskInArray(); //call the loop function to show each task in the array
 
 plusButton.onclick = ()=>{ //when user click on plus button
   let inputValue = inputBox.value; //create a variable to obtain user's input value
-  let obtainLsData = localStorage.getItem("Here is new task"); //create a variable to obtain local storage data
-  if(obtainLsData == null){ //if local storage has no data
-    taskArr = []; //create an empty array
+  let obtainLocalStorageData = localStorage.getItem("Here is new task"); //create a variable to obtain local storage data
+  if(obtainLocalStorageData == null){ //if local storage has no data
+    taskArray = []; //create an empty array
   }else{ // if it has data
-    taskArr = JSON.parse(obtainLsData);  //transform json string into a js object
+    taskArray = JSON.parse(obtainLocalStorageData);  //transform json string into a js object
   }
-  taskArr.push(inputValue); //adding new value into the task array
-  localStorage.setItem("Here is new task", JSON.stringify(taskArr)); //transform java script object into a json string
+  taskArray.push(inputValue); //adding new value into the task array
+  localStorage.setItem("Here is new task", JSON.stringify(taskArray)); //transform java script object into a json string
   loopTaskInArray(); //call the loop function to show each task in the array
 }
  
+function onCheckBoxClick(index) {
+  console.log(index)
+}
+
 function loopTaskInArray() {
-  let obtainLsData = localStorage.getItem("Here is new task"); //create a variable to obtain local storage data
-  if (obtainLsData == null) { //if local storage has no data
-    taskArr = []; //create an empty array
+  let obtainLocalStorageData = localStorage.getItem("Here is new task"); //create a variable to obtain local storage data
+  if (obtainLocalStorageData == null) { //if local storage has no data
+    taskArray = []; //create an empty array
   } else { //if it has data
-    taskArr = JSON.parse(obtainLsData); //transform json string into a js object
+    taskArray = JSON.parse(obtainLocalStorageData); //transform json string into a js object
   }
   let ulTag = ""; //create a string variable to add each task into the array 
-  taskArr.forEach((task, index) => { //create a loop to add each string put in the input with in parameter the task and where to put it the array
-    ulTag += `<li>${task}<span class="icon" onclick="removeTask(${index})"><i class="fas fa-trash"></i></span></li>`; //the ul list with the class, icon, and delete function 
+  taskArray.forEach((task, index) => { //create a loop to add each string put in the input with in parameter the task and where to put it the array
+    ulTag += `<li><input type="checkbox" onclick="onCheckBoxClick(${index})"/>${task}<span class="icon" onclick="removeTask(${index})"><i class="fas fa-trash deleteButton"></i></span></li>`; //the ul list with the class, icon, and delete function 
   });
   checkBox.innerHTML = ulTag; //display the ul list on the actual page
   inputBox.value = ""; //when task added leave the input value empty
@@ -34,10 +38,10 @@ function loopTaskInArray() {
  
 // remove task function
 function removeTask(index) { 
-  let obtainLsData = localStorage.getItem("Here is new task");  //create a variable to obtain local storage data
-  taskArr = JSON.parse(obtainLsData); //transform json string into a js object
-  taskArr.splice(index, 1); //remove the task the array at the given input
-  localStorage.setItem("Here is new task", JSON.stringify(taskArr)); //transform java script object into a json string
+  let obtainLocalStorageData = localStorage.getItem("Here is new task");  //create a variable to obtain local storage data
+  taskArray = JSON.parse(obtainLocalStorageData); //transform json string into a js object
+  taskArray.splice(index, 1); //remove the task the array at the given input
+  localStorage.setItem("Here is new task", JSON.stringify(taskArray)); //transform java script object into a json string
   loopTaskInArray(); //call the loop function to show each task in the array
 }
 
